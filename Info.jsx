05d377ed9cfde492b1d4a56ebcad9479abf328d7
AniketGaikwad42 <70379850+AniketGaikwad42@users.noreply.github.com>
2021-06-26 04:51:32 -0700

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React js</h1>
+      <p>A basic WebDev ReactJS Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
